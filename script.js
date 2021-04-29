59ba30ba1a4c05f6f44a2c78d23d4d4a29923895
shax911 <82878451+shax911@users.noreply.github.com>
2021-04-29 22:31:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var second = +prompt('введите число в секундах');
+alert('перейдите в консоль для просмотра ответа');
+var minute = 60;
+console.log((second / minute) + ' минут')
+var hour = 3600;
+console.log((second / hour) + ' часов')
+var day = 86400;
+console.log((second / day) + ' дней')
